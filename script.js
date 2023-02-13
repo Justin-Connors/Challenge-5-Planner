4f6afb5e45c15f7e7ede3cada19d62e51cdcf447
Justin <justinconnors@hotmail.ca>
2023-02-13 19:45:56 -0400

    time of day added

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 //past / present / future
-$(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -21,4 +20,14 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+$(document).ready(function() {
+
+
+
+
+  //Time of Day
+  setInterval(function() {
+    $("#currentDay").text(dayjs().format('h:mm A'));
+  }, 1000);
+
 });
